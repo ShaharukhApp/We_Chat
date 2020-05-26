40cd933ab3fe05882f252b48b11a5ea3d4132a0a
ShaharukhApp <53910141+ShaharukhApp@users.noreply.github.com>
2020-05-26 17:42:37 +0530

    make changes in server file

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 const io=require('socket.io')(8080)
+var express=require('express');
+var app=express();
+
+const PORT = process.env.PORT || 5000
+
 const users={}
 
 io.on('connection',socket=>{
@@ -15,3 +20,8 @@ io.on('connection',socket=>{
     })
 
 })
+app.get('/',(req,res)=>{
+    res.send('welcome to the world of We Chat');
+    
+})
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
